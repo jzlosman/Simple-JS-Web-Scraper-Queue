eeba567af14390b614aba21939234c5d2cc3ca13
Jeremy Zaborowski <jzlosman@gmail.com>
2019-03-21 01:37:36 -0400

    bump timeout up for the download check for large file

--- test/queue/job.js
+++ test/queue/job.js
@@ -4,13 +4,13 @@ const {assert} = require('chai');
 const db = require('./../../db/db');
 const job = require('../../queue/job');
 
-describe("Web Scraper", () => {
+describe("Web Scraper", async () => {
   it("fetches a URL and stores result in database", async () => {
     const queueId = 'A'
     const url = 'https://google.com'
     const website = db.create(queueId, url);
     const html = '<html><body>Test</body></html>';
-    
+
     //mock url response (intercept url call)
     const mockScrape = nock(url).get('/').reply(200, html);
 
@@ -33,5 +33,5 @@ describe("Web Scraper", () => {
       assert.isNull(result.html);
       assert.match(result.error, /^maxContentLength/, 'error message is not expected.')
     });
-  })
+  }).timeout(4000)
 });
